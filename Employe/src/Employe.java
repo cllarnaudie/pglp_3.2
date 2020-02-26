cdfd5826f06162b6857e7010de30b767d129eb2a
Utilisateur <user@debian.arcahe.ovh>
2020-02-26 16:16:32 +0100

    ajout de la classe Employe et de la classe test

---
+++ Employe/src/Employe.java
@@ -0,0 +1,13 @@
+
+public class Employe {
+	private final String nom; 
+	private final String adresse; 
+	
+	public double calculSalaire () {
+		return /*calcul du salaire */
+	}
+    
+	public void afficheCoordonnees () {
+		System.out.println (nom + "" + adresse); 
+	}
+}
---
+++ Employe/src/EmployeTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+public class EmployeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
